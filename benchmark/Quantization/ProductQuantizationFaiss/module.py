2471b2822eefb7d5f19f52298415400d64141ec7
YPM300 <ym621@scarletmail.rutgers.edu>
2025-11-21 14:24:35 -0500

    Initialization

---
+++ benchmark/Quantization/ProductQuantizationFaiss/module.py
@@ -0,0 +1,20 @@
+import faiss
+import numpy as np
+from typing import Tuple
+class ProductQuantizationFaiss:
+    def __init__():
+        pass
+
+    def fit(nd: int, data: np.ndarray) -> bool:
+        pass 
+    def query(nq: int, query: np.ndarray, topk: int) -> Tuple[np.ndarray, np.ndarray]:
+        pass
+
+    def getMemoryUsage() -> float:
+        pass
+
+    def getCompressionRate() -> float:
+        pass
+    def getMSE() -> float:
+        pass
+    pass
