0d0808c7039c4aa04e19dd2aa02e88cb3de32dc4
YPM300 <ym621@scarletmail.rutgers.edu>
2026-01-13 17:09:21 -0500

    Add MSE and pass basic

--- benchmark/algorithms/quantizer/ExtendedRabitQNTU/module.py
+++ benchmark/algorithms/quantizer/ExtendedRabitQNTU/module.py
@@ -5,22 +5,29 @@ import psutil
 sys.path.insert(0, '/benchmark')
 from benchmark.base import BaseQuantizer
 import ExtendedRabitQ
+import ExtendedRabitQ_HighAcc
 import faiss
 class ExtendedRabitQNTU(BaseQuantizer):
-    def __init__(self, ndim, nbit, data_bytes, nthread = 1):
-        self.Index = ExtendedRabitQ.Index(ndim,nbit)
+    def __init__(self, ndim, nbit, data_bytes, high_acc_flag , nthread = 1):
+        self.high_acc_flag = high_acc_flag
+        if high_acc_flag:
+            self.Index = ExtendedRabitQ_HighAcc.Index(ndim,nbit)
+        else:
+            self.Index = ExtendedRabitQ.Index(ndim,nbit)
         self.ndim = ndim
         self.nbit = nbit
         self.nthread = nthread
         faiss.omp_set_num_threads(nthread)
-
         self.data_bytes = data_bytes 
+
+
+    
 
     def fit(self, nd:int, data: np.ndarray):
         try:
             centroids = np.mean(data, axis = 1)
             cids = np.zeros(nd)
-            self.Index.train(data,centroids,cids,nd, self.nthread)
+            self.Index.train(data,centroids,nd, self.nthread)
         except Exception as e:
             print(f"Training error: {e}")
             return False
@@ -37,11 +44,18 @@ class ExtendedRabitQNTU(BaseQuantizer):
         return psutil.Process().memory_info().rss/1024
 
     def getCompressionRate(self) -> float:
-        return self.nbit / (self.ndim * (self.data_bytes * 8))  
+        return self.nbit / (self.data_bytes * 8)  
 
 
     def getMSE(self) -> float:
-        return self.Index.get_mse()
+        return self.Index.getMSE()
+
+    def set_query(self, query, thread_id) :
+        pass
+
+    def estimate_distance(self, idx, thread_id):
+        pass
+
 
 
 
